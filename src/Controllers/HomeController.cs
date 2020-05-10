64716e54fb069f082dbc44fe56d46402ce02c931
daniel.wong <daniel.wong@xero.com>
2020-05-10 11:19:37 +1000

    Create a simple mvc3 project

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Web.Mvc;
+
+namespace WebApplication14.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            using (var client = new HttpClient())
+            {
+                return Content("OK");
+            }
+        }
+    }
+}
